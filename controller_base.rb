313b350a49837b3a6730b2f42950358d6a4dd658
Nader Arbabian <Nader.arb72@gmail.com>
2019-02-15 12:18:35 -0500

    add session cookies and views

--- controller_base.rb
+++ controller_base.rb
@@ -1,6 +1,9 @@
+require 'active_support/inflector'
+require 'erb'
+require_relative 'session'
 
 class ControllerBase
-  attr_reader :req, :res, :params
+  attr_reader :req, :res, :params,:session
   attr_accessor :response_built
 
   def initialize(req, res)
@@ -12,18 +15,36 @@ class ControllerBase
     @response_built
   end
 
+  def redirect_response_prep
+    raise "Double render error" if self.already_built_response?
+    self.response_built = true
+    self.session.store_session(@res)
+
+  end
+
   def redirect_to(url)
+    self.redirect_response_prep
     @res.status = 302
     @res["Location"] = url
   end
 
   def render_content(content, content_type)
-      raise "Double render error" if self.already_built_response?
+      self.redirect_response_prep
       @res.write(content)
       @res['Content-Type'] = content_type
-      self.response_built = true
 
     nil
+  end
+
+  def render(template_name)
+    path = File.dirname(__FILE__)
+    template_path = File.join(path,"..",'views',self.class.name.underscore,"#{template_name}.html.erb")
+    template = File.read(template_path)
+    render_content(ERB.new(template).result(binding),"text/html")
+  end
+
+  def session
+    @session ||= Session.new(@req)
   end
 
 end
